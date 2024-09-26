03dcc5c2c0e0ccccd5a0081ddf37908106337b96
KSelete <keketsoselete@gmail.com>
2024-09-26 11:51:51 -0700

    debugging

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -3,11 +3,11 @@
 # Increase the ULIMIT of the default file
 exec { 'fix--for-nginx':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => '/usr/bin:/bin'
+  path    => '/usr/local/bin/:/bin/'
 } ->
 
 # Restart Nginx
 exec { 'nginx-restart':
-  command => 'service nginx restart',
-  path    => '/usr/bin:/bin'
+  command => '/etc/init.d/nginx restart',
+  path    => '/sbin:/bin:/usr/sbin:/usr/bin',
 }
